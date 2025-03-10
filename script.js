701376fbdcb5a0df79661c9d55836cb9894eaf32
eliatc <etoco1314@gmail.com>
2025-03-10 09:36:55 +0100

    cambios

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $("#ocultar-btn").click(function(){
+        $("#parrafo").hide();
+    });
+    
+    $("#mostrar-btn").click(function(){
+        $("#parrafo").show();
+    });
+});
